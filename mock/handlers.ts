4fc94ee46207a84739a403a59258b57a2071bd91
mudasar187 <mudasar_cheema@hotmail.com>
2025-08-22 12:49:28 +0200

    rydder opp og bruker msw

--- mock/handlers.ts
+++ mock/handlers.ts
@@ -5,12 +5,10 @@ import skattetrekkInitResponse from "./skattetrekkInitResponse.json";
 export const handlers = [
 
   http.get("/utbetaling/skattetrekk/api/skattetrekk", () => {
-    console.log("Kjører initiate");
     return HttpResponse.json(skattetrekkInitResponse, { status: 200 });
   }),
 
   http.post("/utbetaling/skattetrekk/api/skattetrekk", async () => {
-    console.log("Kjører send");
     return new HttpResponse(null, { status: 200 });
   }),
 ];
