192ae9455e005eff9b5b939314746b7a055965f4
mudasar187 <mudasar_cheema@hotmail.com>
2025-09-24 09:31:27 +0200

    sletter ubrukt kode

--- src/common/FormatKroner.tsx
+++
@@ -1,6 +0,0 @@
-import { numberFormatWithKr } from "../common/Utils";
-import "./FormatKroner.css";
-
-export const FormatKroner = ({ value }: { value: number }) => {
-  return <span id="format-kroner">{numberFormatWithKr(value)}</span>;
-};
